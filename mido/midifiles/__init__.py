e23c5dce9829938b9a9cfd63b5a8f5f16f64c884
Pepe Barbe <dev@antropoide.net>
2017-08-09 00:04:35 -0500

    export additional functions

--- mido/midifiles/__init__.py
+++ mido/midifiles/__init__.py
@@ -1,4 +1,5 @@
 from .meta import MetaMessage, UnknownMetaMessage
 from .units import tick2second, second2tick, bpm2tempo, tempo2bpm
 from .tracks import MidiTrack, merge_tracks
-from .midifiles import MidiFile
+from .midifiles import MidiFile, TimingUnit, TimingReference, \
+    DEFAULT_TEMPO, DEFAULT_TICKS_PER_BEAT, DEFAULT_TIME_SIGNATURE
